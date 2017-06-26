591cffdae2aabd6e9d1a54bfec1c12604b03c5af
samwah <samuel.wahlstedt@gmail.com>
2017-06-26 16:26:31 +0200

    Autocomplete, a complete mess

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 //import 'materialize-css';
 import { MaterializeModule } from 'angular2-materialize';
-import { FormsModule} from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
+import { requestOptionsProvider } from "./default-request-options.service";
 import 'hammerjs';
 
 //material
 //import { MdIconModule } from "@angular/material";
+import { MaterialModule } from '@angular/material';
 
 //Libraries
 import { NguiAutoCompleteModule } from "@ngui/auto-complete";
@@ -21,22 +24,28 @@ import { SearchComponent } from './search/search.component'; //typescripts now k
 //Service
 import { SearchService } from "./search.service";
 
-
 @NgModule({ //Register components so nh module knows
   declarations: [
     AppComponent,
     ServerComponent, //add here (but get error if you have not imported it FIRST)
-    ServersComponent, SearchComponent  
+    ServersComponent, 
+    SearchComponent
   ],
   imports: [ //imports - allows us to add other modules to this module
     BrowserModule,
+    BrowserAnimationsModule,
     //MdIconModule,
     //MaterializeModule,
     HttpModule,
     FormsModule,
-    NguiAutoCompleteModule
+    ReactiveFormsModule,
+    NguiAutoCompleteModule,
+    MaterialModule,
   ],
-  providers: [SearchService],
+  providers: [
+    SearchService,
+    requestOptionsProvider
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,18 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl} from "@angular/forms";
 import { SearchService } from "../search.service";
+import { Observable, Subject } from 'rxjs';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-symptom: string;
-  constructor(private _SearchService: SearchService) { }
+
+  myControl: FormControl;
+
+  searchQuery: string = '';
+  //autocompleteList: Array<string> = [];
+  autocompleteList = ['Loss of Appetite',
+    'Loss of Speech'];
+
+  option: string;
+  options: Array<any> = ['Start typing to see symptoms'];
+  filteredOptions: Observable<string[]>;
+  //theObservable : Observable<any[]> = Observable.of(this.options);
+  //theSubject = new Subject<Array<any>>();
+  //theSubscription = this.theSubject.subscribe(
+  //  data => this.theObservable=Observable.of(data)
+  //)
+  constructor(private _SearchService: SearchService) {
+    this.myControl = new FormControl();
+  }
+
+/*
+  onKey(event: any){
+    console.log(event.target.value);
+    if (event.target !== undefined){
+      this._SearchService.getAutoComplete(event.target.value.toLowerCase())
+        .subscribe(
+          data => this.options = this.responseToArray(data)
+        )
+    }
+  }
+*/
+  getMatches(query){
+    console.log(query);
+    let matches: any[];
+ 
+    this._SearchService.getAutoComplete(query.toLowerCase())
+      .subscribe(
+        data => {
+          console.log('data is:', data);
+          matches = this.responseToArray(data)
+          
+        }
+      )
+
+    return matches;
+  }
+  
+  responseToArray(object) {
+    let myArray = [];
+    if(object.length > 0 && object !== undefined ){
+      let limit = 5;
+      if(object.length < 6){
+        limit = object.length;
+      }
+      for(let i = 0; i < limit; i++){
+        myArray.push(object[i].name);
+      }
+    }
+    return myArray;
+  }
 
   ngOnInit() {
-    this._SearchService.getAutoComplete('loss').subscribe(data => {
-      console.log('Hello!', data);
-    })
+    this.filteredOptions = this.myControl.valueChanges
+         .map( data => { 
+           console.log(this.getMatches(data));
+           return this.getMatches(data);
+          })
   }
 
 }
