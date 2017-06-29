5c51075ae588a5a8a7bf5ae7356ff72d2c99e0e8
amlinden <amlinden@kth.se>
2017-06-29 11:33:46 +0200

    Revert "Merge branch 'master' of https://github.com/amlinden/etsimedforhospital"
    This reverts commit 70e49a3514eae5f4b61b30008a04465cbb76ce0f, reversing
    changes made to 43c5f67e7cf42d4bc07db478c1016c1d06c39981.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 //import 'materialize-css';
 import { MaterializeModule } from 'angular2-materialize';
 import { FormsModule} from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import 'hammerjs';
+import { Routes, RouterModule } from '@angular/router';
+import {MdRadioModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 //material
 //import { MdIconModule } from "@angular/material";
 
-//Libraries
-import { NguiAutoCompleteModule } from "@ngui/auto-complete";
+
 
 //components
 import { AppComponent } from './app.component';
@@ -19,25 +21,28 @@ import { ServersComponent } from './servers/servers.component';
 import { SearchComponent } from './search/search.component';
 import { AgeComponent } from './age/age.component'; //typescripts now know where to find it and it can be used
 
-//Service
-import { SearchService } from "./search.service";
-
+const appRoutes: Routes = [
+  { path: '', component: AppComponent}, //what should happen - component
+  { path: 'age', component: AgeComponent},
+  { path: 'search', component: SearchComponent},
+]; //all routes, has to follow structure
 
 @NgModule({ //Register components so nh module knows
   declarations: [
     AppComponent,
-    ServerComponent, //add here (but get error if you have not imported it FIRST)
-    ServersComponent, SearchComponent  
+    ServerComponent, //add here (but get error i you have not imported it FIRST)
+    ServersComponent, SearchComponent, AgeComponent  
   ],
   imports: [ //imports - allows us to add other modules to this module
     BrowserModule,
     //MdIconModule,
     //MaterializeModule,
-    HttpModule,
     FormsModule,
-    NguiAutoCompleteModule
+    MdRadioModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [SearchService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchService } from "../search.service";
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-symptom: string;
-  constructor(private _SearchService: SearchService) { }
+  symptom: string;
+  onSubmit(form:NgForm){
+    
+  }
+
+  constructor() { }
 
   ngOnInit() {
-    this._SearchService.getAutoComplete('loss').subscribe(data => {
-      console.log('Hello!', data);
-    })
   }
 
 }
